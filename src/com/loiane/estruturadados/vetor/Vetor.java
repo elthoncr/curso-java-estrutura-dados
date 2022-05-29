30d30f7177c7f3c26a24eca7a76c2bba64a0a014
Elthon Rodrigues <elthon.c.r@gmail.com>
2022-05-29 19:42:38 -0300

    Aula 04 Verificando tamanho do Array e imprimir elementos do vetor utilizando StringBuilder

--- src/com/loiane/estruturadados/vetor/Vetor.java
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -1,5 +1,7 @@
 package com.loiane.estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
 
 	private String[] elementos;
@@ -37,4 +39,32 @@ public class Vetor {
 		} 
 		return false;
 	}
+	
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder s = new StringBuilder();
+		
+		s.append("[");
+		
+		for (int i=0; i<this.tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho-1]);
+		}
+		
+		s.append("]");
+		
+		return s.toString();
+	}
+	
+	
+	
 }
