1f6daed0a3c59557a51ac30b33c6b3002a51d2ce
Elthon Rodrigues <elthon.c.r@gmail.com>
2022-05-29 18:32:19 -0300

    Aula 03 - Vetores e Arrays 2

---
+++ src/com/loiane/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("Elemento 1");
+		vetor.adiciona("Elemento 2");
+
+	}
+
+}
