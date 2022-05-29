47fc5897b9c15d1da1d21e8d68e3d8ca8ce97ea5
Elthon Rodrigues <elthon.c.r@gmail.com>
2022-05-29 19:10:19 -0300

    Aula 03 - Vetores e Arrays Parte 3

--- src/com/loiane/estruturadados/vetor/Vetor.java
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -3,17 +3,38 @@ package com.loiane.estruturadados.vetor;
 public class Vetor {
 
 	private String[] elementos;
+	private int tamanho; 
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
 	}
 	
-	public void adiciona(String elemento) {
+	/*public void adiciona(String elemento) {
 		for (int i = 0; i < this.elementos.length; i++) {
 			if(this.elementos[i] == null) {
 				this.elementos[i] = elemento;
 				break;
 			}
 		}
+	}*/
+	
+	/*public void adiciona(String elemento) throws Exception{
+		
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		} else {
+			throw new Exception("Vetor já esta cheio, não é possivel adicionar mais elementos.");
+		}
+		
+	}*/
+	
+	public boolean adiciona(String elemento) {
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		} 
+		return false;
 	}
 }
--- src/com/loiane/estruturadados/vetor/teste/Aula03.java
+++ src/com/loiane/estruturadados/vetor/teste/Aula03.java
@@ -5,11 +5,12 @@ import com.loiane.estruturadados.vetor.Vetor;
 public class Aula03 {
 
 	public static void main(String[] args) {
-		
-		Vetor vetor = new Vetor(5);
-		
+
+		Vetor vetor = new Vetor(2);
+
 		vetor.adiciona("Elemento 1");
 		vetor.adiciona("Elemento 2");
+		vetor.adiciona("Elemento 3");
 
 	}
 
